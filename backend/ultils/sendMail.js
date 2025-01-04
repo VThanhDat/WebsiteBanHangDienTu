a233129b6b52ab1d75310d9b9f5565d7b881bc1d
VThanhDat <121113462+VThanhDat@users.noreply.github.com>
2025-01-04 10:34:59 +0700

    update Mail

--- backend/ultils/sendMail.js
+++ backend/ultils/sendMail.js
@@ -14,7 +14,7 @@ const sendMail = asyncHandler(async ({ email, html }) => {
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"FashionStore" <no-relply@fashionstore.com>', // sender address
+    from: '"ElectronicStore" <no-relply@electronicstore.com>', // sender address
     to: email, // list of receivers
     subject: "Forgot password", // Subject line
     html: html, // html body
