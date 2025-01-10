55539df83a9e0c1602c47ab899fa6b3ad61a561b
VThanhDat <121113462+VThanhDat@users.noreply.github.com>
2025-01-10 12:56:59 +0700

    update order model

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -1,30 +1,47 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var orderSchema = new mongoose.Schema({
-  products: [
-    {
-      product: { type: mongoose.Types.ObjectId, ref: "Product" },
-    },
-  ],
-  status: {
-    type: String,
-    default: "Processing",
-    enum: [
-      "Cancelled",
-      "Accepted",
-      "Shipping",
-      "Processing",
-      "Returning",
-      "Success",
+var orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: { type: mongoose.Types.ObjectId, ref: "Product" },
+        quantity: Number,
+        variant: [{ label: String, variant: String }],
+      },
     ],
+    status: {
+      type: String,
+      default: "Processing",
+      enum: [
+        "Cancelled",
+        "Accepted",
+        "Shipping",
+        "Processing",
+        "Returning",
+        "Success",
+      ],
+    },
+    total: Number,
+    coupon: {
+      type: mongoose.Types.ObjectId,
+      ref: "Coupon",
+    },
+    address: {
+      type: String,
+      require: true,
+    },
+    phone: {
+      type: String,
+      require: true,
+    },
+    orderBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
   },
-  paymentIntent: {},
-  orderBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 //Export the model
 module.exports = mongoose.model("Order", orderSchema);
